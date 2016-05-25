b3e1ae6ac7284ffa916c5480c0226361ba05ba5f
jisong <js@jss-MacBook-Pro.local>
2016-05-25 13:35:43 +0800

    variabel 只要赋值value ，订阅就会进入

--- RxSwiftLearn/RxSwiftLearn/VariableViewController.swift
+++ RxSwiftLearn/RxSwiftLearn/VariableViewController.swift
@@ -9,31 +9,54 @@
 import UIKit
 import RxSwift
 
+class Person{
+    var name: String
+    init(name: String){
+        self.name = name
+    }
+}
+
 class VariableViewController: UIViewController {
 
+    private let bag = DisposeBag()
 //    var variable: Variable<String?>?
     override func viewDidLoad() {
         super.viewDidLoad()
     
 
-        let variable: Variable? = Variable<String?>(nil)
+//        let variable: Variable? = Variable<String?>(nil)
+//        
+////        variable!.value = "A"
+//        
+//        _ = variable!.asObservable().subscribe({ (event) in
+//            
+//            switch event{
+//            case let .Next(element):
+//                print(element ?? "nil")
+//            case let .Error(error):
+//                print(error)
+//            case .Completed:
+//                print("complete")
+//            
+//            }
+//        })
+//        variable?.value = "B"
+////        (variable?.asObservable() as! BehaviorSubject).on(.Completed)
         
-//        variable!.value = "A"
         
-        _ = variable!.asObservable().subscribe({ (event) in
-            
-            switch event{
-            case let .Next(element):
-                print(element ?? "nil")
-            case let .Error(error):
-                print(error)
-            case .Completed:
-                print("complete")
-            
-            }
-        })
-        variable?.value = "B"
-//        (variable?.asObservable() as! BehaviorSubject).on(.Completed)
+        let variable2 = Variable<Person?>(nil)
+        variable2.asObservable().subscribeNext { (person) in
+            print(person?.name)
+        }.addDisposableTo(bag)
+        
+        variable2.asObservable().subscribe { (event) in
+            print(event)
+        }.addDisposableTo(bag)
+        
+        let p = Person(name: "zhangsan")
+        variable2.value = p
+        p.name = "lisi"
+        variable2.value = p
         
     }
 
